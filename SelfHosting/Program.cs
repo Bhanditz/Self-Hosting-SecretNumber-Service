c5d1ce8bf6357b403b21ad8972c3a28642b3e6ed
Sean Casaus <shawn.casaus@gmail.com>
2018-06-03 20:33:59 -0700

    First Commit

---
+++ SelfHosting/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace SelfHosting
+{
+    [ServiceContract]
+    //use interface to define contracts
+    public interface myInterface
+    {
+        [OperationContract]
+        string CheckNumber(int userNum, int SecretNum);
+
+        [OperationContract]
+        int SecretNumber(int lower, int upper);
+    }
+
+    //implementation of interface
+    public class guessService : myInterface
+    {
+        public int SecretNumber(int lower, int upper)
+        {
+            DateTime currentDate = DateTime.Now;
+            int seed = (int)currentDate.Ticks;
+            Random random = new Random(seed);
+            int sNumber = random.Next(lower, upper);
+            return sNumber;
+        }
+
+        public string CheckNumber(int userNum, int SecretNum)
+        {
+            if (userNum == SecretNum)
+                return "correct";
+            else
+            if (userNum > SecretNum)
+                return "too big";
+            else return "too small";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a URI instance to guessService as the base address
+            Uri baseAddress = new Uri("http://localhost:8000/Service");
+
+            //create a new ServiceHost instance to host the service
+            ServiceHost selfHost = new ServiceHost(typeof(guessService), baseAddress);
+
+            try
+            {
+                //add a service endpoint with contract and binding
+                selfHost.AddServiceEndpoint(typeof(myInterface), new WSHttpBinding(), "guessService");
+
+                //add metadata for platform-independent access
+                System.ServiceModel.Description.ServiceMetadataBehavior smb = new System.ServiceModel.Description.ServiceMetadataBehavior();
+                //enable the metadata
+                smb.HttpGetEnabled = true;
+                //add here
+                selfHost.Description.Behaviors.Add(smb);
+
+                //start the service and waiting for request
+                selfHost.Open();
+                Console.WriteLine("Running on port 8000, ready to take requests");
+                Console.WriteLine("Call SecretNumber(int lower, int upper) to generate a Secret Number");
+                Console.WriteLine("Call CheckNumber(int userNum, int SecretNum) to guess the secret number");
+                Console.WriteLine("Press <Enter>.\n to quit");
+                Console.ReadLine();
+                //close the ServiceHostBase to shutdown service
+                selfHost.Close();
+            }
+            catch(CommunicationException ce)
+            {
+                Console.WriteLine("An Exception occured: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
---
+++ WcfClientConsole/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WcfClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a proxy to the WCF service
+            int SecretNumber = 0;
+            myInterfaceClient myPxy = new myInterfaceClient();
+
+            //call the service operations through the proxy
+            Console.WriteLine("Enter a low number: ");
+            Int32 low = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a high number: ");
+            Int32 high = Convert.ToInt32(Console.ReadLine());
+
+            SecretNumber = myPxy.SecretNumber(low, high);
+            //use to display secret number you cheater
+            //Console.WriteLine(SecretNumber);
+
+            Console.WriteLine();
+            Int32 numAttempts = 0;
+            bool isCorrect = false;
+            Int32 guess = 0;
+            string result = "";
+
+            while(!isCorrect)
+            {
+                Console.WriteLine("Guess a Number!");
+                guess = Convert.ToInt32(Console.ReadLine());
+                result = myPxy.CheckNumber(guess, SecretNumber);
+
+                if (result == "too big" || result == "too small")
+                {
+                    numAttempts++;
+                    Console.WriteLine("Your Guess was " + result);
+                    Console.WriteLine("Attempts: " + numAttempts);
+                }
+                else if(result == "correct")
+                {
+                    Console.WriteLine("Congrats, your Guess was " + result);
+                    Console.WriteLine("Total Attempts: " + numAttempts);
+                    isCorrect = true;
+                    break;
+                }
+            }
+            
+
+            //Close the Proxy
+            myPxy.Close();
+            Console.WriteLine("/nPress <Enter> to terminate the client./n");
+        }
+    }
+}
